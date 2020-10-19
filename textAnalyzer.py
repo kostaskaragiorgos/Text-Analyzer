4d75b9e42a4e391fbe247da311a1dc32c4833ba6
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-10-19 12:05:01 +0300

    Update textAnalyzer.py

--- textAnalyzer.py
+++ textAnalyzer.py
@@ -1,10 +1,20 @@
+""" text Analyzer """
 from tkinter import Menu, messagebox as msg, filedialog, Tk
 import csv
 from nltk  import tokenize
 from analyze import showcharacters, showcharactersex
 
 
+def helpmenu():
+    """ help menu function """
+    msg.showinfo("Help", "Use the file menu to insert, close  a file and to save the analysis. \nUse the show menu to show the analysis results")
+
+def aboutmenu():
+    """ about menu function """
+    msg.showinfo("About", "Version 1.0")
+
 class TextAnalyzer():
+    """ Text Analyzer class """
     def __init__(self, master):
         self.master = master
         self.master.title("TextAnalyzer")
@@ -13,8 +23,10 @@ class TextAnalyzer():
         self.filename = ""
         self.menu = Menu(self.master)
         self.file_menu = Menu(self.menu, tearoff=0)
-        self.file_menu.add_command(label="Insert File", accelerator='Ctrl+O', command=self.insert_txt)
-        self.file_menu.add_command(label="Close File", accelerator='Ctrl+F4', command=self.closefile)
+        self.file_menu.add_command(label="Insert File", accelerator='Ctrl+O',
+                                   command=self.insert_txt)
+        self.file_menu.add_command(label="Close File", accelerator='Ctrl+F4',
+                                   command=self.closefile)
         self.file_menu.add_command(label="Save as", accelerator='Ctrl+S', command=self.saveas)
         self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
         self.menu.add_cascade(label="File", menu=self.file_menu)
@@ -26,11 +38,11 @@ class TextAnalyzer():
         self.menu.add_cascade(label="Show", menu=self.show_menu)
 
         self.about_menu = Menu(self.menu, tearoff=0)
-        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
+        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=aboutmenu)
         self.menu.add_cascade(label="About", menu=self.about_menu)
 
         self.help_menu = Menu(self.menu, tearoff=0)
-        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
@@ -39,11 +51,10 @@ class TextAnalyzer():
         self.master.bind('<Control-s>', lambda event: self.saveas())
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
         self.master.bind('<Control-r>', lambda event: self.showcharacters())
-        self.master.bind('<Alt-r>', lambda event:  self.shownumberofwords())
+        self.master.bind('<Alt-r>', lambda event: self.shownumberofwords())
         self.master.bind('<Control-u>', lambda event: self.showcharactersex())
-        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
-        self.master.bind('<Control-i>', lambda event: self.aboutmenu())
-    
+        self.master.bind('<Control-F1>', lambda event: helpmenu())
+        self.master.bind('<Control-i>', lambda event: aboutmenu())
     def saveas(self):
         """ saves the analysis """
         if self.filename == "":
@@ -114,15 +125,11 @@ class TextAnalyzer():
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
     
-    def helpmenu(self):
-        pass
-    
-    def aboutmenu(self):
-        """ about menu function """
-        msg.showinfo("About", "Version 1.0")
+
         
 
 def main():
+    """ main function """
     root = Tk()
     TextAnalyzer(root)
     root.mainloop()
